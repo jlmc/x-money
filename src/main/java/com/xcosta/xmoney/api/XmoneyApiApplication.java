f31ebe1c06c598a41f7b01a2f9f3f18c5c1eb2c7
Joao Costa <costa@omega.home>
2017-08-16 20:54:06 +0100

    initial project 3.1

---
+++ src/main/java/com/xcosta/xmoney/api/XmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.xcosta.xmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmoneyApiApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmoneyApiApplication.class, args);
+	}
+}
