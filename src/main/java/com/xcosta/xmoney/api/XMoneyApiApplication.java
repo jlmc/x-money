e73765a39a3b21e39677a0bcb05de2ea9135b0d8
costa <costajlmpp@gmail.com>
2017-08-17 01:12:07 +0100

    3.9. Exception Handler

--- src/main/java/com/xcosta/xmoney/api/XMoneyApiApplication.java
+++ src/main/java/com/xcosta/xmoney/api/XMoneyApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XmoneyApiApplication {
+public class XMoneyApiApplication {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(XmoneyApiApplication.class, args);
+		SpringApplication.run(XMoneyApiApplication.class, args);
 	}
 }
