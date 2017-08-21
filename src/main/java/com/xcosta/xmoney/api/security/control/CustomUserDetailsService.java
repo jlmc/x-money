99f223de7731d4015574dffca2357f47e036095a
costa <costajlmpp@gmail.com>
2017-08-21 15:57:19 +0100

    7.5. add user-infos to JWT Access_token

--- src/main/java/com/xcosta/xmoney/api/security/control/CustomUserDetailsService.java
+++ src/main/java/com/xcosta/xmoney/api/security/control/CustomUserDetailsService.java
@@ -1,23 +1,24 @@
 package com.xcosta.xmoney.api.security.control;
 
-import com.xcosta.xmoney.api.security.entity.Role;
+import com.xcosta.xmoney.api.security.entity.CustomUserPrincipal;
 import com.xcosta.xmoney.api.security.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
     @Autowired
-    UserRepository userRepository;
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -26,7 +27,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     private UserDetails toUserDetails(User user) {
-
+        /*
         Set<SimpleGrantedAuthority> grantedAuthorities = user.getRoles().stream()
                 .map(Role::getName)
                 .map(String::toUpperCase)
@@ -34,5 +35,7 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .collect(Collectors.toSet());
 
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+        */
+        return CustomUserPrincipal.of(user);
     }
 }
---
+++ src/main/java/com/xcosta/xmoney/api/security/control/CustomUserPrincipalTokenTokenEnhancer.java
@@ -0,0 +1,26 @@
+package com.xcosta.xmoney.api.security.control;
+
+import com.xcosta.xmoney.api.security.entity.CustomUserPrincipal;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomUserPrincipalTokenTokenEnhancer implements TokenEnhancer {
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+        CustomUserPrincipal customUserPrincipal = (CustomUserPrincipal) authentication.getPrincipal();
+
+        Map<String, Object> userInfos = new HashMap<>();
+        userInfos.put("name", customUserPrincipal.getUser().getName());
+
+        DefaultOAuth2AccessToken defaultOAuth2AccessToken = (DefaultOAuth2AccessToken) accessToken;
+        defaultOAuth2AccessToken.setAdditionalInformation(userInfos);
+
+        return accessToken;
+    }
+}
